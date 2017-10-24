5abf7911295c9e9e8bcc6b9c960983c7713ea8f9
nico.skuballa <nico.skuballa@add-solution.de>
2017-10-24 13:11:23 +0200

    Git Test Commit and Push

--- src/hallo/HalloWelt.java
+++ src/hallo/HalloWelt.java
@@ -11,6 +11,7 @@ public class HalloWelt {
 		x.setMarke("San Miguel");
 //		x.setKannManTrinken(false);
 		x.setProzent(0);
+		System.out.println("Git Test");
 		System.out.println("Art: " + x.getArt() + "\n" + "Marke: " + x.getMarke() + "\n" + "Prozent: " + x.getProzent() + "%" + "\n" + "Trinkbar: " + x.kannManDasTrinken());
 	}
 }
